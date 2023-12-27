4a964d6f259196aacb23235f6bd003982c3d3d9c
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2023-12-27 11:08:52 +0530

    utils updated

--- videotube/src/db/index.js
+++ videotube/src/db/index.js
@@ -7,24 +7,16 @@ import {DB_NAME} from "../constants.js";
 const connectDB = async () => {
     try {
         dotenv.config();
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        }, ()=>{
-            console.log(`Connected to ${DB_NAME} database`);
-        });
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`Connected to ${DB_NAME} database !! ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.log("mongoDB connection FAILED",error);
         process.exit(1);
     }
 };
 
 
 export default connectDB;
-
-
 
 
 
