e7f1cc43177b8c8020dfd5fbdea4839797be24a8
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2023-12-29 16:28:57 +0530

    Add getUserChannelProfile and getWatchHistory endpoints

--- videotube/src/controllers/user.controller.js
+++ videotube/src/controllers/user.controller.js
@@ -303,6 +303,76 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200,user,"coverImage updated successfully"));
 })
 
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const {username}=req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(400,"username is required");
+    }
+
+    const channel=await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },{
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },{
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },{
+            $addFields:{
+                subscribersCount:{$size:"$subscribers"},
+                channelsSubscribedToCount:{$size:"$subscribedTo"},
+                isSubscribed:{
+                    $cond:{
+                        if:{$eq:[{$size:"$subscribers"},0]},
+                        then:false,
+                        else:{
+                            $in:[req.user?._id,"$subscribers.subscriber"]
+                        }
+                    }
+                }
+
+            }
+        },{
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,   
+                email:1,
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel not exists");
+    }
+
+
+    return res.status(200).
+    json(new ApiResponse(200,channel[0],"Channel fetched successfully"));
+
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    
+})
+
 export {
   registerUser,
   loginUser,
@@ -313,4 +383,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
--- videotube/src/routes/user.routes.js
+++ videotube/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from 'express';
-import {registerUser,loginUser,logoutUser, refreshAccessToken} from '../controllers/user.controller.js';
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -19,5 +31,13 @@ router.route("/login").post(loginUser)
 //secured route
 router.route("/logout").post( verifyJWT, logoutUser)
 router.route("/refreshToken").post( refreshAccessToken)
+router.route("/changePassword").post( verifyJWT, changeCurrentPassword)
+router.route("/currentUser").get( verifyJWT, getCurrentUser)
+router.route("/updateAccountDetails").post( verifyJWT, updateAccountDetails)
+
+router.route("/updateUserAvatar").post( verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/updateUserCoverImage").post( verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/getUserChannelProfile/:username").get( getUserChannelProfile)
+router.route("/getWatchHistory").get( verifyJWT, getWatchHistory)
 
 export default router;
